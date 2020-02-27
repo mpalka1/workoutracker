cb13a5cbed64295ba3d8b2291a50fbece1a35ce1
Matthew Palka <palkamatthew@gmail.com>
2020-02-26 21:40:42 -0600

    add html routes

---
+++ Develop/routes/api.js
@@ -0,0 +1,89 @@
+const router = require("express").Router();
+const Workout = require("./../models/workout");
+
+/* add a new workout */
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/:id", (req, res) => {
+  Workout.findOne({ _id: req.params.id })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.update(
+    {
+      _id: req.params.id
+    },
+    {
+      $push: {
+        exercises: [
+          {
+            type: req.body.type,
+            name: req.body.name,
+            duration: req.body.duration,
+            weight: req.body.weight,
+            reps: req.body.reps,
+            sets: req.body.sets,
+            distance: req.body.distance
+          }
+        ]
+      }
+    }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/workouts/:id", (req, res) => {
+  Workout.findByIdAndRemove(req.params.id)
+    .exec()
+    .then(doc => {
+      if (!doc) {
+        return res.status(404).end();
+      }
+      return res.status(204).end();
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
---
+++ Develop/routes/html_routes.js
@@ -0,0 +1,18 @@
+//routes for stats and exersize
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/index.html'));
+});
+
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/stats.html'));
+});
+
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './../public/exercise.html'));
+});
+
+module.exports = router;
